6063e220f9b1e9a6f954828991765d8cabd74287
Mike McGowan <mike.mcgowan@intoglobal.com>
2016-09-28 17:02:52 +0100

    Fixed some deprecation warnings

--- urls.py
+++ urls.py
@@ -1,14 +1,13 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from oauth2_provider.views import TokenView
 
 from .views import AuthorizationView, UserDataView, oauth2_logout
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^authorize/$', AuthorizationView.as_view(), name='authorize'),
     url(r'^token/$', TokenView.as_view(), name='token'),
     url(r'^user/$', UserDataView.as_view(), name='userdata'),
     url(r'^logout/$', oauth2_logout, name='oauth2_logout')
-)
+]
